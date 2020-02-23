92328594b439cd37c80cc3b52ab8e1857b1211e0
Ryan Chesnut <ryanjchesnut@gmail.com>
2020-02-22 19:45:49 -0500

    init 3

---
+++ precache-manifest.9a69b74bbb705452f238f60872ee583a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7a655d632700a995035058bf9eced2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f1724ab7dd079a051ec",
+    "url": "/static/css/main.7f527e1e.chunk.css"
+  },
+  {
+    "revision": "05610b9e2d7ad567fd1b",
+    "url": "/static/js/2.3bc992c7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.3bc992c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1724ab7dd079a051ec",
+    "url": "/static/js/main.50d49771.chunk.js"
+  },
+  {
+    "revision": "063acc87b9fa00ca0f9f",
+    "url": "/static/js/runtime-main.157a780a.js"
+  }
+]);
